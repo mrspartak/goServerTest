eb6ab31de732496b7bc94589ced1ea2aa60f2db9
Assorium <mr.spartak@rambler.ru>
2015-03-11 21:09:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const resp = `<html>
+    <head>
+        <title>Simple Web App</title>
+    </head>
+    <body>
+        <h1>Simple Web App</h1>
+        <p>Hello World!</p>
+    </body>
+</html>`
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("server", "Go")
+	fmt.Fprintf(w, resp)
+	fmt.Fprintf(w, r.URL.String())
+}
 
 func main() {
-    fmt.Printf("hello, world 123\n")
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":9000", nil)
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
