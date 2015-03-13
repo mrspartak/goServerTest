c4f73cfaaef07bf66826d968745035fb877e76ef
Spartak <assorium@gmail.com>
2015-03-13 22:02:30 +0300

    martini

--- main.go
+++ main.go
@@ -1,34 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
-
-const resp = `<html>
-    <head>
-        <title>Simple Web App</title>
-    </head>
-    <body>
-        <h1>Simple Web App</h1>
-        <p>Hello World!</p>
-    </body>
-</html>`
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("server", "Go")
-	fmt.Fprintf(w, resp)
-	fmt.Fprintf(w, r.URL.String())
-}
+import "github.com/go-martini/martini"
 
 func main() {
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8000", nil)
-
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Run()
 }
